6335d0cddb52f707fd1e316932f5a21e4f3b6b42
ab-costa <costa.alessandroborges@gmail.com>
2022-02-05 20:46:40 -0300

    Solution uri 1044

---
+++ Uri1044.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Uri1044 {
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+
+        int a, b;
+
+        a = teclado.nextInt();
+        b = teclado.nextInt();
+
+        if(a > b && a % b == 0) {
+            System.out.println("Sao Multiplos");
+        } else if (a < b && b % a == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+        teclado.close();
+    }
+}
